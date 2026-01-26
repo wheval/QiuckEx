de0616c4422147732dd187c48e1b8262f0e333d1
Dairus <dairusokoh@gmail.com>
2026-01-26 20:41:48 +0100

    Add TransactionsModule to AppModule imports

--- app/backend/src/app.module.ts
+++ app/backend/src/app.module.ts
@@ -6,22 +6,19 @@ import { HealthModule } from './health/health.module';
 import { SupabaseModule } from './supabase/supabase.module';
 import { UsernamesModule } from './usernames/usernames.module';
 import { NotificationService } from './notifications/notification.service';
-
 import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
-  imports: [AppConfigModule, SupabaseModule, HealthModule, UsernamesModule, TransactionsModule],
-})
-export class AppModule { }
   imports: [
     EventEmitterModule.forRoot({
       wildcard: true,
       delimiter: '.',
     }),
-    AppConfigModule, 
-    SupabaseModule, 
-    HealthModule, 
-    UsernamesModule
+    AppConfigModule,
+    SupabaseModule,
+    HealthModule,
+    UsernamesModule,
+    TransactionsModule,
   ],
   providers: [NotificationService],
 })
